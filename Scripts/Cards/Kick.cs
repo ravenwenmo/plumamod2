26388c8490c95a4029067ad187e2344a176d2d9f
ravenwenmo <152868743+ravenwenmo@users.noreply.github.com>
2026-08-07 16:30:31 +0800

    新增踢击和下意识

---
+++ Scripts/Cards/Kick.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Cards;
+using MegaCrit.Sts2.Core.ValueProps;
+using STS2RitsuLib.Cards.DynamicVars;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace Pluma.Scripts;
+
+// 踢击：1费攻击牌，造成8点伤害，抽1张牌。本能。升级后伤害9，抽2张。
+[RegisterCard(typeof(PlumaCardPool))]
+public class Kick : ModCardTemplate
+{
+    private const int energyCost = 1;
+    private const CardType type = CardType.Attack;
+    private const CardRarity rarity = CardRarity.Common; // 可根据需要调整
+    private const TargetType targetType = TargetType.AnyEnemy;
+    private const bool shouldShowInCardLibrary = true;
+
+    public override CardAssetProfile AssetProfile => new(
+        PortraitPath: $"res://pluma/images/cards/{GetType().Name}.png"
+    );
+
+    // 本能关键词
+    public override IEnumerable<CardKeyword> CanonicalKeywords => new[]
+    {
+        MyKeywords.MuscleMemory
+    };
+
+    // 动态变量：基础伤害8，抽牌数1
+    protected override IEnumerable<DynamicVar> CanonicalVars => new DynamicVar[]
+    {
+        new DamageVar(8m, ValueProp.Move),
+        new CardsVar(1)
+    };
+
+    public Kick() : base(energyCost, type, rarity, targetType, shouldShowInCardLibrary)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        // 造成伤害
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
+            .FromCard(this, cardPlay)
+            .Targeting(cardPlay.Target!)
+            .Execute(choiceContext);
+
+        // 抽牌
+        await CardPileCmd.Draw(choiceContext, DynamicVars.Cards.BaseValue, base.Owner);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(1m);   // 伤害 8 → 9
+        DynamicVars.Cards.UpgradeValueBy(1m);    // 抽牌 1 → 2
+    }
+}
---
+++ Scripts/Cards/Subconscious.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Cards;
+using MegaCrit.Sts2.Core.ValueProps;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace Pluma.Scripts;
+
+// 下意识：1费技能牌，获得8点格挡。本能。升级后格挡+3。
+[RegisterCard(typeof(PlumaCardPool))]
+public class Subconscious : ModCardTemplate
+{
+    private const int energyCost = 1;
+    private const CardType type = CardType.Skill;
+    private const CardRarity rarity = CardRarity.Common;
+    private const TargetType targetType = TargetType.Self;
+    private const bool shouldShowInCardLibrary = true;
+
+    public override CardAssetProfile AssetProfile => new(
+        PortraitPath: $"res://pluma/images/cards/{GetType().Name}.png"
+    );
+
+    // 本能关键词
+    public override IEnumerable<CardKeyword> CanonicalKeywords => new[]
+    {
+        MyKeywords.MuscleMemory
+    };
+
+    // 格挡变量
+    protected override IEnumerable<DynamicVar> CanonicalVars => new[]
+    {
+        new BlockVar(8m, ValueProp.Move)
+    };
+
+    public Subconscious() : base(energyCost, type, rarity, targetType, shouldShowInCardLibrary)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await CreatureCmd.GainBlock(
+            base.Owner.Creature,
+            DynamicVars.Block.BaseValue,
+            ValueProp.Move,
+            cardPlay
+        );
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Block.UpgradeValueBy(3m); // 8 → 11
+    }
+}
