4db3237cde65c1aef8860ceecf2299eb47b193d5
ravenwenmo <152868743+ravenwenmo@users.noreply.github.com>
2026-08-09 02:58:15 +0800

    友情羁绊改成了先古牌

--- Scripts/Cards/Friendship.cs
+++ Scripts/Cards/Friendship.cs
@@ -19,7 +19,7 @@ public class Friendship : ModCardTemplate
 {
     private const int energyCost = 1;
     private const CardType type = CardType.Skill;
-    private const CardRarity rarity = CardRarity.Rare;
+    private const CardRarity rarity = CardRarity.Ancient;
     private const TargetType targetType = TargetType.Self;
     private const bool shouldShowInCardLibrary = true;
 
